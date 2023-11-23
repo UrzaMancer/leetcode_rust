92c89cb4767f4a8fa2e96830cd77705a3a4d9466
Matt J <1829173+UrzaMancer@users.noreply.github.com>
2023-11-23 00:26:41 -0800

    1630 arithmetic-subarrays solved + random test gen
    Randomly generated 10 test cases using a manual test case generation function. This should be moved to a separate module, with data structures to group constraints and ranges based on the problem description and parameters.
    
    The test case generation simply prints out the test cases in the same format as the leetcode source, when using `cargo run` you can redirect the output to a text file and copy from there to the leetcode ui.
    
    The manual test harness does not yet read test cases from file: that's still a WIP.
    
    For the problem solution, we have function `is_arithmetic(range)` which takes a slice from the nums vec, that calculates the difference of adjacent numbers in the slice (right minus left), and then returns true if all those differences are equal. Note the fold iterator adaptor starts special case None, and the rest of the diffs are handled in Option to avoid magic numbers.
    
    Because the problem states we can rearrange any of the ranges, the most likely solutin is to sort the range, as if it is possible for the range to be an arithmetic subarray, and all differences will be equal, this will be reflected in a nondecreasing sequence. If a range isn't arithmetic, then we can attempt to sort the range to see if that sorted range is arithmetic, using `sort_range(range)`
    
    Finally, to iterate through all the range start/end pairs contained in `l` and `r`, we zip them together (they are required to be same length, `m`), map the subsequence bounded by the range from nums, then run the `is_arithmetic` check on the range and if that fails, then on the sorted range, returning the `bool` result for each range pair, collecting them together and returning the collection.
    
    10ms 83.33%
    2.11 MB 83.33%

--- 1630_arithmetic-subarrays/src/main.rs
+++ 1630_arithmetic-subarrays/src/main.rs
@@ -1,4 +1,5 @@
 mod solution;
+use rand::{thread_rng, Rng};
 
 pub struct Solution;
 
@@ -17,13 +18,33 @@ macro_rules! test_case {
     }
 }
 
+fn generate_testcase(ct: usize, constraint_ranges: &mut Vec<std::ops::RangeInclusive<i32>>)
+    -> (String, (Vec<i32>, Vec<i32>, Vec<i32>)) {
+    let mut rng = thread_rng();
+    let (n, m) = (rng.gen_range(constraint_ranges[0].clone()), rng.gen_range(constraint_ranges[1].clone()));
+    let nums: Vec<i32> = (0..n).map(|_| rng.gen_range(constraint_ranges[2].clone())).collect();
+    let r: Vec<i32> = (0..m).map(|_| rng.gen_range(1..n)).collect();
+    let l: Vec<i32> = r.iter().map(|&right| rng.gen_range(0..right)).collect();
+    (
+        format!("tc{}", ct).to_string(), 
+        (nums, l, r)
+    )
+}
+
 
 test_case!(check_arithmetic_subarrays: 
            tc0, (([4,6,5,9,3,7], [0,0,2], [2,3,5]), [true,false,true]),
             tc1, (([-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], [0,1,6,4,8,7], [4,4,9,7,9,10]), [false,true,false,false,true,true]),
         );
 
+
 fn main() {
+    (0..10).for_each(|count| {
+        let (_, (nums, l, r)) = 
+        generate_testcase(count+2, &mut vec![2..=500, 1..=500, -100_000..=100_000]);
+        println!("{:?}\n{:?}\n{:?}\n", nums, l, r);
+    });
+
     /*
     let file = File::open(TESTCASE_FILE).expect(format!("find and open {} ", TESTCASE_FILE).as_str());
 
--- 1630_arithmetic-subarrays/src/solution.rs
+++ 1630_arithmetic-subarrays/src/solution.rs
@@ -2,9 +2,32 @@ use crate::Solution;
 
 impl Solution {
     pub fn check_arithmetic_subarrays(nums: Vec<i32>, l: Vec<i32>, r: Vec<i32>) -> Vec<bool> {
-        if nums.len() | l.len() | r.len() == 0 {
-            return vec![];
-        }
-        vec![false]
+        l.iter().zip(r.iter())
+            .map(|(&left, &right)| &nums[left as usize..=right as usize])
+            .map(|range|
+                 Self::is_arithmetic(range)
+                 || Self::is_arithmetic(&Self::sort_range(range))
+            ).collect()
+    }
+
+    fn sort_range(range: &[i32]) -> Vec<i32> {
+        let mut sorted_vec: Vec<i32> = range.to_vec();
+        sorted_vec.sort_unstable();
+        sorted_vec
+    }
+
+    fn is_arithmetic(range: &[i32]) -> bool {
+        print!("{:?}", range);
+        range.len() > 1 &&
+        range.iter().zip(range[1..].iter())
+            .map(|(&prev, &num)| num - prev)
+            .fold((true, None), |(equal_diff, last): (bool, Option<i32>), diff|
+                  (
+                      equal_diff 
+                      && (last.is_none()
+                      || last.unwrap() == diff),
+                      Some(diff)
+                    )
+                  ).0
     }
 }
